35c654296558c374574c0c36d7157ab48d260345
Tash-had Saqif <me@tash-had.com>
2019-02-13 19:03:48 -0500

    rename res lock

--- a2/safeStopSign.h
+++ a2/safeStopSign.h
@@ -32,7 +32,7 @@ typedef struct _SafeStopSign {
 	pthread_mutex_t eLock;
 	pthread_mutex_t wLock;
 
-	pthread_mutex_t laneLock;
+	pthread_mutex_t reservationLock; // only one thread should be able to make reservations at a time
 
 	pthread_cond_t northLaneCV;
 	pthread_cond_t southLaneCV;
